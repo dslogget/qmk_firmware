d9c7e96836d404da19e1d6fd228241e61c612de4
Daniel Sloggett <dslogget@gmail.com>
2024-01-22 01:13:46 +0000

    add some new keys I've been meaning to

--- keyboards/splitkb/aurora/sofle_v2/keymaps/dslogget/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/dslogget/keymap.c
@@ -46,21 +46,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        XXXXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F24,  KC_MUTE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TG(_GAMING),
-               KC_LGUI, KC_LALT, KC_LSFT, KC_ENT,  MO_SYM,    MO_NAV,  KC_SPC,  KC_BSPC, KC_LCTL, XXXXXXX
+        KC_SPC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F24,  KC_MUTE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TG(_GAMING),
+               KC_LGUI, KC_LALT, KC_LSFT, KC_ENT,  MO_SYM,    MO_NAV,  KC_SPC,  KC_BSPC, KC_LCTL, KC_PRINT_SCREEN
     ),
     [_GAMING] = LAYOUT(
          KC_5, KC_ESC,  KC_1, KC_2, KC_3, KC_4,                      _______, _______, _______, _______, _______, _______,
          KC_T, KC_TAB,  KC_Q, KC_W, KC_E, KC_R,                      _______, _______, _______, _______, _______, _______,
          KC_G, KC_LSFT, KC_A, KC_S, KC_D, KC_F,                      _______, _______, _______, _______, _______, _______,
          KC_B, KC_LCTL,   KC_Z, KC_X, KC_C, KC_V, _______,      _______, _______, _______, _______, _______, _______, _______,
-               KC_LALT, KC_LSFT, KC_LCTL, KC_SPC, MO_DIS_GAME,     _______, _______, _______, _______, _______
+               KC_LSFT, KC_LALT, KC_LCTL, KC_SPC, _______,     _______, _______, _______, _______, _______
     ),
     [_NAVIGATION] = LAYOUT(
         KC_F12,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                             KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, _______,                   _______, _______, KC_UP, KC_PGUP, KC_PGUP, _______,
+        _______, KC_MEDIA_PLAY_PAUSE, KC_KP_7, KC_KP_8, KC_KP_9, _______,                   _______, _______, KC_UP, KC_PGUP, KC_PGUP, _______,
         _______, KC_KP_0, KC_KP_4, KC_KP_5, KC_KP_6, _______,                   KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, _______,
-        _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______, _______, _______, KC_PGDN, _______, _______,
+        KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______, _______, _______, KC_PGDN, _______, _______,
                           _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
     ),
     [_SYMBOLS] = LAYOUT(
@@ -143,6 +143,11 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_VOLD);
         }
     } else if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_WH_U);
+        } else {
+            tap_code(KC_WH_D);
+        }
     }
 
     return false;
